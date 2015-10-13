45837326db23543797f6ec8055755e94a9f6971a
Amoriello Hutti <amoriello.hutti@gmail.com>
2015-10-13 23:13:18 +0200

    fixing error __cxa_guard_acquire/release/abort when using static variables (Ex: new Keyboard and HID libraries).

---
+++ src/token/fake_cxa_guards.cc
@@ -0,0 +1,6 @@
+extern "C" {
+  __extension__ typedef int __guard __attribute__((mode (__DI__))); 
+  int __cxa_guard_acquire(__guard *g) { return !*(char *)(g); };
+  void __cxa_guard_release (__guard *g) { *(char *)g = 1; };
+  void __cxa_guard_abort (__guard *) { }; 
+} // end extern "C"
