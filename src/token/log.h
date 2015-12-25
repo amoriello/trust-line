8e23d2fa58d83351a3534bd782ac2c991777fa6d
Amoriello Hutti <amoriello.hutti@gmail.com>
2015-12-25 17:49:23 +0100

    Adding "ENABLE_TLOG" macro to enable / disable logging.

--- src/token/log.h
+++ src/token/log.h
@@ -1,15 +1,19 @@
 //  Author Amoriello Hutti - 2015
 
+#ifndef SRC_TOKEN_LOG_H_
+#define SRC_TOKEN_LOG_H_
+
 #include <token/log/logger.h>
 
+#ifdef ENABLE_TLOG
+  #define TLOG_INIT() g_log.Intialize()
+  #define TLOG(...) g_log.println(__VA_ARGS__)
+  #define TLOG_(...) g_log.print(__VA_ARGS__)
+# else
+  #define TLOG_INIT()
+  #define TLOG(...)
+  #define TLOG_(...)
+#endif  //  ENABLE_TLOG
 
-//#define TLOG(...) g_log.println(__VA_ARGS__)
 
-//#define TLOG_(...) g_log.print(__VA_ARGS__)
-
-#define TLOG2(...)  g_log.println(__VA_ARGS__)
-
-#define TLOG(...)
-#define TLOG_(...)
-
-#define TLOG_INIT() g_log.Intialize()
+#endif  // SRC_TOKEN_LOG_H_
